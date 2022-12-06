c1690b919d27e7167a72e193c18114085db0adec
M-Sandberg <marcus@stickerapp.com>
2022-12-06 19:24:42 +0100

    no msg

--- day6/main.py
+++ day6/main.py
@@ -1,11 +1,11 @@
 
-def part_one(msg, unique_req):
+def part_one_and_two(msg, unique_req):
     for idx in range(0, len(msg), 1):
         if len(set(msg[idx:idx+unique_req])) == len(msg[idx:idx+unique_req]):
             return idx + unique_req
 
 with open('./list.txt', 'r') as day6_data:
     msg = day6_data.readlines()[0]
-    print(part_one(msg, 4))
-    print(part_one(msg, 14))
+    print(part_one_and_two(msg, 4))
+    print(part_one_and_two(msg, 14))
 
